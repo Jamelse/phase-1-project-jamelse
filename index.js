c4bc225e2fb020111091fd902f192eb957046d35
Jacob <jacobamelse@hotmail.com>
2022-09-03 21:36:50 -0500

    added random quote api to quote render

--- index.js
+++ index.js
@@ -1,19 +1,82 @@
-const mainApi = 'https://api.gameofthronesquotes.xyz/v1/random';
+const gotApi = 'https://api.gameofthronesquotes.xyz/v1/random';
 const imgApi = 'https://api.got.show/api/show/characters';
+
 // --Nodes-- //
-const container = document.querySelector('.container')
-const mainDiv1 = document.querySelector('#main')
+
+const container = document.querySelector('.container');
+const mainDiv = document.getElementById('main');
+const quoteLink = document.getElementById('quoteLink');
+const resetLink = document.querySelector('#resetLink');
+
+// --Handlers-- //
+
+function mainReset(){
+  mainDiv.innerHTML = '';
+}
 
 function homePage(){
+  mainReset();
   // --Element Creation-- //
-  let h1 = document.createElement('h1');
-  let p = document.createElement('p');
+  const img = document.createElement('img')
+  const h1 = document.createElement('h1');
   
   // --Element innerText-- //
+  img.src = './Game of Thrones/Game-of-Thrones-logo.png';
+  img.className = 'responsive-img';
   h1.innerText = 'Thrones Quote Generator';
-  p.innerText = 'to be determined quote';
   
   // --Element Appending-- //
-  mainApi.appendChild(h1);
-  mainApi.appendChild(p);
+  mainDiv.appendChild(img);
+  mainDiv.appendChild(h1);
+  
 }
+
+function renderQuote(quote){
+ mainReset();
+  
+  const h2 = document.createElement('h2');
+  const ul = document.createElement('ul');
+  const li = document.createElement('li');
+  
+  h2.innerText = quote.character.name;
+  li.innerText = `"${quote.sentence}"`;
+  
+
+  ul.appendChild(li);
+  mainDiv.appendChild(h2);
+  mainDiv.appendChild(ul);
+  
+}
+
+function randomQuote(){
+  fetch (gotApi)
+  .then(resp => resp.json())
+  .then(data => {
+    renderQuote(data)
+  })
+}
+
+// --Event Listeners-- //
+
+function quoteLinkClickEvent(){
+  quoteLink.addEventListener('click', (e) => {
+    e.preventDefault;
+    randomQuote();
+    //characterImg();
+  })
+}
+
+function resetLinkClickEvent(){
+  resetLink.addEventListener('click', (e) => {
+    e.preventDefault;
+    homePage();
+  })
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  homePage();
+  quoteLinkClickEvent();
+  resetLinkClickEvent();
+})
