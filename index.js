480ea026efb68f2ed30fa525be9c0013713e5cb7
Jacob <jacobamelse@hotmail.com>
2022-09-04 02:08:54 -0500

    added image functionality with the random quote generator, also added new font

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function homePage(){
   // --Element innerText-- //
   img.src = './Game of Thrones/Game-of-Thrones-logo.png';
   img.className = 'responsive-img';
-  h1.innerText = 'Thrones Quote Generator';
+  h1.innerText = 'Quote Generator';
   
   // --Element Appending-- //
   mainDiv.appendChild(img);
@@ -33,36 +33,72 @@ function homePage(){
 
 function renderQuote(quote){
  mainReset();
-  
   const h2 = document.createElement('h2');
   const ul = document.createElement('ul');
   const li = document.createElement('li');
   
   h2.innerText = quote.character.name;
   li.innerText = `"${quote.sentence}"`;
-  
-
+ 
   ul.appendChild(li);
   mainDiv.appendChild(h2);
   mainDiv.appendChild(ul);
   
 }
-
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 function randomQuote(){
   fetch (gotApi)
   .then(resp => resp.json())
   .then(data => {
+    //data.character.name
+    characterImage(data.character.name)
     renderQuote(data)
+    characterHouse(data.character.house.slug)
   })
 }
+
+function characterImage(char){
+  fetch(`https://api.got.show/api/show/characters`)
+  .then(resp => resp.json())
+  .then(img =>{
+    let image = document.createElement('img')
+    for (let i = 0; i<img.length; i++){
+    if (img[i].name.includes(char)){
+    image.src = img[i].image;
+    mainDiv.appendChild(image)
+      } 
+    }
+  })
+ }
+
+function characterHouse(char){
+  fetch(`https://api.got.show/api/show/houses`)
+  .then(resp => resp.json())
+  .then(logo => {
+    let img = document.createElement('img')
+    img.className = "houseLogo";
+    for (let x = 0; x<logo.length; x++){
+      //console.log(logo[x].name)}
+      if (logo[x].name.toLowerCase().includes(char))
+       img.src = logo[x].logoURL
+        mainDiv.appendChild(img);
+      
+    }
+  })
+}
+
 
 // --Event Listeners-- //
 
 function quoteLinkClickEvent(){
   quoteLink.addEventListener('click', (e) => {
     e.preventDefault;
+    characterHouse();
     randomQuote();
-    //characterImg();
+    characterImage();
+    
   })
 }
 
